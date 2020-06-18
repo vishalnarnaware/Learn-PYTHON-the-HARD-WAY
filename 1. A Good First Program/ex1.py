a10d0062ee572c16944114a854e64dd51d6e4124
vishalnarnaware <narnaware_vishal.ghrceai@raisoni.net>
2020-06-18 12:48:27 +0530

    Exercise 1

---
+++ 1. A Good First Program/ex1.py
@@ -0,0 +1,19 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+# Study Drills
+# 1. Make your script print another line.
+print("This is another line :)")
+
+# 2. Make your script print only one of the lines.
+print("Comment all the lines except one...\n",
+      "To make script print only one of the lines.")
+
+# 3. Put a “#” (octothorpe) character at the beginning of a line. What did it do?
+# Try to find out what this character does.
+print("The line above this code didn't make a change in the output.")
