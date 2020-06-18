02224836bbe2444e5d72e110af505ff3a1653757
vishalnarnaware <narnaware_vishal.ghrceai@raisoni.net>
2020-06-18 12:49:28 +0530

    Exercise 3

---
+++ 3. Numbers and Math/ex3.py
@@ -0,0 +1,44 @@
+print("I will now count my chickens:")
+
+print("Hens", 25+ 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+
+# Study Drills
+# 1. Above each line, use the # to write a comment to yourself explaining what the line does.
+# Strongly recommended to do
+
+# 2. Remember in Exercise 0 when you started Python? Start Python this way again and, using
+# the above characters and what you know, use Python as a calculator.
+# All on you!
+
+# 3. Find something you need to calculate and write a new .py file that does it.
+# print("My career: ", 100 - 1 / 0.001 + 6 * 2 % 13 + 11.1 * 10)
+
+# 4. Notice the math seems “wrong”? There are no fractions, only whole numbers.
+# Find out why by researching what a “floating point” number is.
+# Actually I got floating numbers... So looks like some advantage of Python 3 :P
+
+# Rewrite ex3.py to use floating point numbers so it’s more accurate.
+# (hint: 20.0 is floating point).
+# Instead you could try to convert this to integer. Hint:
+# print(int(3/5))
