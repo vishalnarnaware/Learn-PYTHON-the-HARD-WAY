1ce16502d88a0ed800c42147dd5bf5ea6a29d0f3
vishalnarnaware <narnaware_vishal.ghrceai@raisoni.net>
2020-07-11 09:33:49 +0530

    Exercise 34

---
+++ 34. Accessing Elements of Lists/ex34.py
@@ -0,0 +1,18 @@
+animals = ['bear', 'tiger', 'penguin', 'zebra']
+bear = animals[0]
+print("Animals list: ", animals)
+print("Animal at index 0:", bear)
+
+# practice
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print("Animals list: ", animals)
+
+print("1. The animal at 1.", animals[1])
+print("2. The third animal.", animals[3-1])    # ordinal
+print("3. The first animal.", animals[0])      # ordinal
+print("4. The animal at 3.", animals[3])
+print("5. The fifth animal.", animals[4])      # ordinal
+print("6. The animal at 2. %s" % animals[2])
+print("7. The sixth animal. %s" % animals[5])  # ordinal
+print("8. The animal at 4. %s" % animals[4])
