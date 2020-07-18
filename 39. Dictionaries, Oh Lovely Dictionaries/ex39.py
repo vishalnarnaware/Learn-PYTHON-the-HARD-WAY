c143f8a1748e6b97de392ac25186ab41886651f0
vishalnarnaware <narnaware_vishal.ghrceai@raisoni.net>
2020-07-18 23:04:17 +0530

    Fixed Errors

--- 39. Dictionaries, Oh Lovely Dictionaries/ex39.py
+++ 39. Dictionaries, Oh Lovely Dictionaries/ex39.py
@@ -35,17 +35,17 @@ print("Florida has: ", cities[states['Florida']])
 
 # print every state abbreviation
 print('-' * 10)
-for state, abbrev in state.items():
+for state, abbrev in states.items():
     print("%s is abbreviated %s" % (state, abbrev))
 
 # print every city in state
 print('-' * 10)
-for abbrev, city in city.items():
+for abbrev, city in cities.items():
     print("%s has the city %s" % (abbrev, city))
 
 # now do both at the same time
 print('-' * 10)
-for state, abbrev in state.items():
+for state, abbrev in states.items():
     print("%s state is abbreviated %s and has city %s" % (
         state, abbrev, cities[abbrev]))
 
