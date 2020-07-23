af4b950654fabd735b53f989b9fd185aaa1fed39
vishalnarnaware <narnaware_vishal.ghrceai@raisoni.net>
2020-07-23 16:06:46 +0530

    Question Comment Exercise

---
+++ 42. Is-A, Has-A, Objects, and Classes/ex42.py
@@ -0,0 +1,20 @@
+## Animal is-a object (yes, sort of confusion) look at the extra credit
+class Animal(object):
+    pass
+
+## ??
+class Dog(Animal):
+
+    def __init__(self, name):
+        ## ??
+        self.name = name
+
+## ??
+class Cat(Animal):
+
+    def __init__(self, name):
+        ## ??
+        self.name = name
+
+## ??
+class Per
