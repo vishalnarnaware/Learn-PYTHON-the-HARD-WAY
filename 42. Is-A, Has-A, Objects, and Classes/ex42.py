8a0eed87da6ee9cc283a905ec56d3aacdb76fb2d
vishalnarnaware <narnaware_vishal.ghrceai@raisoni.net>
2020-07-29 20:41:35 -0500

    Classes and objects

--- 42. Is-A, Has-A, Objects, and Classes/ex42.py
+++ 42. Is-A, Has-A, Objects, and Classes/ex42.py
@@ -43,5 +43,34 @@ class Fish(object):
 class Salmon(Fish):
     pass
 
+## ??
+class Halibut(Fish):
+    pass
 
-    
+
+## rover is-a Dog
+rover = Dog("Rover")
+
+## ??
+satan = Cat("Satan")
+
+## ??
+mary = Person("Mary")
+
+## ??
+mary.pet = satan
+
+## ??
+frank = Employee("Frank", 120000)
+
+## ??
+frank.pet = rover
+
+## ??
+flipper = Fish()
+
+## ??
+crouse = Salmon()
+
+## ??
+harry = Halibut()
