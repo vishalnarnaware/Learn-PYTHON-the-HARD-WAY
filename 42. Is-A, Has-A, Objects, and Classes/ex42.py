48e46147aff2282ad58f6063bcfffcd044bdb886
vishalnarnaware <narnaware_vishal.ghrceai@raisoni.net>
2020-07-29 20:35:28 -0500

    Classes done

--- 42. Is-A, Has-A, Objects, and Classes/ex42.py
+++ 42. Is-A, Has-A, Objects, and Classes/ex42.py
@@ -17,4 +17,31 @@ class Cat(Animal):
         self.name = name
 
 ## ??
-class Per
+class Person(object):
+
+    def __init__(self, name):
+        ## ??
+        self.name = name
+
+        ## Person has-a pet of some kind
+        self.pet = None
+
+## ??
+class Employee(Person):
+
+    def __init__(self, name, salary):
+        ## ?? hmm what is this strange magic?
+        super(Employee, self).__init__(name)
+        ## ??
+        self.salary = salary
+
+## ??
+class Fish(object):
+    pass
+
+## ??
+class Salmon(Fish):
+    pass
+
+
+    
