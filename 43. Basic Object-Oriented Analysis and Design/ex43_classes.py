479a1eaa3175f471768cf37356e01d49bfdbaf16
vishalnarnaware <narnaware_vishal.ghrceai@raisoni.net>
2020-07-29 20:56:48 -0500

    Gothon Game Class Hierarchy

--- 43. Basic Object-Oriented Analysis and Design/ex43_classes.py
+++ 43. Basic Object-Oriented Analysis and Design/ex43_classes.py
@@ -26,3 +26,30 @@ class LaserWeaponArmory(Scene):
 
     def enter(self):
         pass
+
+class TheBridge(Scene):
+
+    def enter(self):
+        pass
+
+class EscapePod(Scene):
+
+    def enter(self):
+        pass
+
+
+class Map(object):
+
+    def __init__(self, start_scene):
+        pass
+
+    def next_scene(self, scene_name):
+        pass
+
+    def opening_scene(self):
+        pass
+
+
+a_map = Map('central_corridor')
+a_game = Engine(a_map)
+a_game.play()
