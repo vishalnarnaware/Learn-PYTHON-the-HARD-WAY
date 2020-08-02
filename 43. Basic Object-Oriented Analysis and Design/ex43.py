f16eb182ff3ed670c03bf52f11ae586103dc1bae
vishalnarnaware <narnaware_vishal.ghrceai@raisoni.net>
2020-08-01 19:44:11 -0700

    Central Corridor with actions

--- 43. Basic Object-Oriented Analysis and Design/ex43.py
+++ 43. Basic Object-Oriented Analysis and Design/ex43.py
@@ -44,3 +44,36 @@ class CentralCorridor(Scene):
         print ("a Gothon jumps out, red scaly skin, dark grimy teeth, and evil clown costume")
         print ("flowing around his hate filled body. He's blocking")
         print ("Armory and about to pull a weapon to blast you.")
+
+        action = input("> ")
+
+        if action == "shoot!":
+            print ("Quick on the draw you yank out your blaster and fire it at the Gothon.")
+            print ("His clown costume is flowing and moving around his body, which throws")
+            print ("off your aim. Your laser hits his costume but misses him entirely. This")
+            print ("completely ruins his brand new costume his mother bought him, which")
+            print ("makes him fly into a rage and blast you repeatedly in the face until")
+            print ("you are dead. Then he eats you.")
+            return 'death'
+
+        elif action == 'dodge!':
+            print ("Like a world class boxer you dodge, weave, slip and slide right")
+            print ("as the Gothon's blaster cranks a laser past your head.")
+            print ("In the middle of your artful dodge your foot slips and you")
+            print ("bang your head on the metal wall and pass out.")
+            print ("You wake up shortly after only to die as the Gothon stomps on")
+            print ("your head and eats you.")
+            return 'death'
+
+        elif action == "tell a joke":
+            print ("Lucky for you they made you learn Gothon insults in the academy.")
+            print ("You tell the one Gothon joke you know:")
+            print ("Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr, fur fvgf nebhaq gur ubhfr.")
+            print ("The Gothon stops, tries not to laugh, then busts out laughing and can't move.")
+            print ("While he's laughing you run up and shoot him square in the head")
+            print ("putting him down, then jump through the Weapon Armory door.")
+            return 'laser_weapon_armory'
+
+        else:
+            print ("DOES NOT COMPUTE!")
+            return 'central_corridor'
